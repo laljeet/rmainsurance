361effc33f8347b2d69a326740970c58c3912d18
laljeet <lsangha@clemson.edu>
2024-02-07 16:49:52 -0500

    naming change

--- data-raw/Originial_data.R
+++ data-raw/Originial_data.R
@@ -2,6 +2,26 @@
 library(tidyverse)
 library(data.table)
 
+
+
+
+zip_dir <- paste0(getwd(),"/data-raw")
+#
+# # Get a list of all zip files in the directory
+# zip_files <- list.files(path = zip_dir, pattern = "*.zip")
+#
+# # Unzip and then remove each file
+# for (zip_file in zip_files) {
+#   # Create the full file path
+#   zip_file_path <- file.path(zip_dir, zip_file)
+#
+#   # Unzip the file
+#   unzip(zip_file_path, exdir = zip_dir)
+#
+#   # Remove the zip file
+#   file.remove(zip_file_path)
+# }
+
 # Get info on the data from the word file on website
 
 
@@ -12,23 +32,6 @@ col_info <- data.frame(
   "Description" = c("The identifier that represents the year in which the crop/commodity is normally harvested and indicates the policy year for which coverage was provided.", "The FIPS code that denotes the State in which the insured farm is located.", "USPS state abbreviation", "The FIPS code that denotes the County in which the insured farm is located.", "Name of the county.", "The Risk Management Agency (RMA) code that denotes the crop/commodity for which the policy is issued.", "Name of the crop/commodity", "Code that denotes the type of insurance coverage is selected for the insured crop (e.g.APH, Revenue, Dollar, etc.)", "Abbreviation of the Insurance Plan Name", "Code that identifies the category of coverage elected : A = Buyup ; C = CAT ; E = Existing Coverage Policy ; L = Limited Coverage", "See Stage Code Listing", "The RMA code that identifies the peril that caused the loss on the crop", "Description of the cause of loss.", "Month when the loss occurred", "Name of the month when the loss occurred", "Year the loss occurred", "Count of the number of crop policies with premium  NOTE : Not available for years prior to 1989.", "Count of the number of crop policies with a reported loss", "Number of acres, tons, pounds, etc. planted to the crop after the insured’s share is applied.", "Number of acres insured under an endorsement (e.g. SCO, STAX, Margin Protection)", "The maximum dollar amount of insurance for the crop", "Premium before application of any subsidies.  In general, the base premium rates times liability (Also called the base premium)", "Premium paid by the producer", "Amount of subsidized premium.", "Subsidy provided by a state or private entity", "An additional subsidy from a program or other process. 2021 and 2022 Additional Subsidy represents Pandemic Cover Crop Program amount.", "EFA Premium Discount", "Number of acres, tons, pounds, etc. loss due to damage after the insured’s share is applied", "The total amount of the loss for the designated peril.", "Calculated: Indemnity / Total Premium.")
 )
 
-
-zip_dir <- paste0(getwd(),"/data-raw")
-
-# Get a list of all zip files in the directory
-zip_files <- list.files(path = zip_dir, pattern = "*.zip")
-
-# Unzip and then remove each file
-for (zip_file in zip_files) {
-  # Create the full file path
-  zip_file_path <- file.path(zip_dir, zip_file)
-
-  # Unzip the file
-  unzip(zip_file_path, exdir = zip_dir)
-
-  # Remove the zip file
-  file.remove(zip_file_path)
-}
 
 # Get a list of all text files in the directory
 txt_files <- list.files(path = zip_dir, pattern = "*.txt")
@@ -60,6 +63,10 @@ rename_based_on_year <- function(dlist) {
     year <- df[1, 1]
     # Construct the new name by replacing the numeric part with the extracted year
     new_name <- sub("\\d+", as.character(year), name)
+
+    # Remove .txt from the end of the name
+    new_name <- sub("\\.txt$", "", new_name)
+
     # Return the new name
     return(new_name)
   })
